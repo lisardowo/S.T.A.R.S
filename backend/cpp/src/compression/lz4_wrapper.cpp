0430321dd4fd2ba36ae5834087bafe46e6e9f6d6
pavelsaso <pavelbaufue@gmail.com>
2026-01-03 23:00:40 -0600

    Add C++ LZ4 compression, fragmentation, XOR, checksum. <Complete Draft>

--- backend/cpp/src/compression/lz4_wrapper.cpp
+++ backend/cpp/src/compression/lz4_wrapper.cpp
@@ -1,3 +1,26 @@
 #include "lz4_wrapper.h"
+#include "../../external/lz4/lib/lz4.h"
 
-// later usage
+std::vector<char>compressBlock(const std::vector<char>& input) {        //compression
+    int maxSize = LZ4_compressBound(input.size());
+    std::vector<char> compressed(maxSize);
+    int compressedSize = LZ4_compress_default(
+        input.data(),
+        compressed.data(),
+        input.size(),
+        maxSize
+    );
+    compressed.resize(compressedSize);
+    return compressed;
+}
+
+std::vector<char>decompressBlock(const std::vector<char>& compressed, int originalSize) {       //decompression
+    std::vector<char> output(originalSize);
+    LZ4_decompress_safe(
+        compressed.data(),
+        output.data(),
+        compressed.size(),
+        originalSize
+    );
+    return output;
+}
--- backend/cpp/src/utils/telemetry_generator.cpp
+++ backend/cpp/src/utils/telemetry_generator.cpp
@@ -4,27 +4,22 @@
 
 int main() {
     std::ofstream file("data/input/telemetry.csv");
-
     file << "time,altitude,velocity,accel_x,accel_y,accel_z,temperature,voltage\n";
-
     std::mt19937 rng(1);
     std::normal_distribution<double> noise(0.0, 0.02);
+    double altitude = 800000.0; // meters
+    double velocity = 7777.7;   // m/s
+    double temperature = 0.0;   // celsius
+    double voltage = 10.0;        // volts
 
-    double altitude = 400000.0; // metros (órbita baja)
-    double velocity = 7777.0;   // m/s
-    double temperature = 22.0;
-    double voltage = 3.7;
-
-    for (int i = 0; i < 200000; ++i) {  // ~200k líneas ≈ 1.3 MB
+    for (long long i = 0; i < 20000; ++i) {  // ~2000k lines, 0.01 MB at least
         altitude += velocity * 0.01 + noise(rng);
         velocity += noise(rng);
         temperature += noise(rng);
-        voltage += noise(rng) * 0.001;
-
+        voltage += noise(rng) * 0.01;
         double ax = std::sin(i * 0.01) + noise(rng);
         double ay = std::cos(i * 0.01) + noise(rng);
-        double az = -9.81 + noise(rng);
-
+        double az = noise(rng);
         file << i << ","
              << altitude << ","
              << velocity << ","
